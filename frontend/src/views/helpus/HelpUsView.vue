c3ee1e8ba27b7159340ef3d8d447d578946bccc2
Iury de Mattos <iurydemattos@gmail.com>
2023-11-23 18:47:55 -0300

    feat: helpus view update

---
+++ frontend/src/views/helpus/HelpUsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="full-height">
+    <div class="helpus full-height q-mx-xl">
+      <h1>Ajude a Causa</h1>
+      <p>Esse é um projeto filantrópico com objetivo de ajudar todos aqueles que desejam estudar,
+        seja para melhorar o desempenho escolar, seja para passar em um processo seletivo ou
+        ter conhecimento em alguma área de estudo.
+        <hr />
+        <a href="https://github.com/rufuziu/atividadeextensionista" target="_blank">Repositório GitHub</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.helpus {
+  display: flex;
+  align-items: center;
+}
+
+.helpus>p {
+  text-align: center;
+}
+</style>
