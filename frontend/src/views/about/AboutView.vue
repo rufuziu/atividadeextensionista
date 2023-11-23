781c4ce6c0ec6665a16be214b798f107f715a778
Iury de Mattos <iurydemattos@gmail.com>
2023-11-23 18:48:14 -0300

    feat: about view update

---
+++ frontend/src/views/about/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="full-height">
+    <div class="about full-height q-mx-xl">
+      <h1>Projeto da Atividade Extensionista</h1>
+      <p>Esse é um projeto para realização da atividade extensionista do curso de bacharelado
+        em engenharia de software realizado pelo aluno Iury de Mattos RA 3989685.
+        <hr />
+        <a href="https://github.com/rufuziu/atividadeextensionista" target="_blank">Repositório GitHub</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style setup>
+  .about {
+    display: flex;
+    align-items: center;
+  }
+
+  .about>p {
+    text-align: center;
+  }
+</style>
